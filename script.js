ec576e9b31d43451f8f148aee39d5736bcb16a8b
Atakan Akgun <47303791+atakien@users.noreply.github.com>
2024-08-26 19:28:34 +0300

    Added opacity and random color functionalities
    Random color is created using Math.random values
    for rgb color codes. opacity is set to 0.1
    initially and increased incrementally with every
    interaction up to 1. Event listeners are used for
    buttons.

--- script.js
+++ script.js
@@ -1,31 +1,90 @@
-const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-const r = randomBetween(0, 255);
-const g = randomBetween(0, 255);
-const b = randomBetween(0, 255);
-const rgb = `rgb(${r},${g},${b})`;
-
-
 let container = document.querySelector(".container");
 
-for (i=0; i < 16; i++) {
-    let row = container.appendChild(document.createElement("div"));
-    row.setAttribute("class", "row");
-    for (j=0; j < 16; j++) {
-            let square = row.appendChild(document.createElement("div"));
-            square.setAttribute("class", "square");
-     }
+const resetButton = document.querySelector(".reset");
+
+const gridSize = document.querySelector(".size");
+
+let randomColor = document.querySelector(".color");
+
+
+
+function removeGrid(){
+    let rows = document.querySelectorAll(".row");
+    let squares =document.querySelectorAll(".square")
+    rows.forEach(row => {
+        row.remove();
+      });
+    squares.forEach(square => {
+        square.remove();
+    })
+};
+
+
+function createGrid(gridEdge){
+    removeGrid()
+    for (i=0; i < gridEdge; i++) {
+        let row = container.appendChild(document.createElement("div"));
+        row.setAttribute("class", "row");
+        for (j=0; j < gridEdge; j++) {
+                let square = row.appendChild(document.createElement("div"));
+                square.setAttribute("class", "square");
+                square.style.opacity = 0.1;
+         }
+    }
+    changeColor();
 }
 
-function changeColor  () {
+window.addEventListener("load", () => {
+    createGrid(16);
+})
+
+
+function changeColor() {
+    let hoverColor = "black";
     const squareEl = document.querySelectorAll(".square")
     squareEl.forEach(squares => {
         squares.addEventListener("mouseover",(e)=>{
-            e.target.style.backgroundColor = "black";
+            e.target.style.backgroundColor = hoverColor;
+            let currentOpacity = parseFloat(e.target.style.opacity)
+            e.target.style.opacity = currentOpacity + 0.1;
         });
     });
 }
-changeColor();
 
+gridSize.addEventListener("click", () => {
+    let userChoice = prompt("Enter the number between 2 and 100");
+    let userNumber = Number(userChoice);
+
+    if (Number.isInteger(userNumber) && userNumber >= 2 && userNumber <= 100) {
+        createGrid(userNumber);
+    } else {
+        alert("Please choose an integer number between 2 and 100.");
+    }
+})
+
+function getRandomColor(){
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    hoverColor = `rgb(${r},${g},${b})`;
+}
+
+randomColor.addEventListener("click", () => {
+    const squareEl = document.querySelectorAll(".square")
+    squareEl.forEach(squares => {
+        squares.addEventListener("mouseover",(e)=>{
+            getRandomColor();
+            e.target.style.backgroundColor = hoverColor;
+        });
+    });
+})
+
+resetButton.addEventListener("click", () => {
+    let userChoice = prompt("Enter the number between 2 and 100");
+    let userNumber = Number(userChoice);
+    createGrid(userNumber);
+})
 
 
 
